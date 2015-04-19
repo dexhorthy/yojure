c8fe4af0df22dfdf222ec4eef236d280305cd0c9
Dex <dexter@sproutsocial.com>
2015-04-19 16:41:30 -0500

    echo

--- src/yojure/web.clj
+++ src/yojure/web.clj
@@ -6,13 +6,18 @@
   [clojure.java.io :as io]
   [ring.adapter.jetty :as jetty]
   [environ.core :refer [env]]))
+
 (defn splash []
-{:status 200
-:headers {"Content-Type" "text/plain"}
-:body "Yo"})
+  {:status 200
+  :headers {"Content-Type" "text/plain"}
+  :body "Yo"})
+
+(defn send-yo [username token]
+  (client/post "http://api.justyo.co/yo/" {:query-params {:username username :api_token token}}))
 
 (defn yo-handler [params]
   (println "got request with params" params)
+  (send-yo (params :username) (env :yo-token))
   {:status 200 :body "whatever"})
 
 (defroutes app
@@ -21,12 +26,7 @@
   (GET "/yo" {params :params}
     (yo-handler params)))
 
-(defn send-yo [username token]
-  (client/post "http://api.justyo.co/yo/" {:query-params {:username username :api_token token}}))
 
-(defn -main []
-  (send-yo "ZENSABUROU" (env :YO_TOKEN)))
-
-;(defn -main [& [port]]
-;  (let [port (Integer. (or port (env :port) 3000))]
-;    (jetty/run-jetty (site #'app) {:port port :join? false})))
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 3000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
